a6265b299fdfc432205a9c2c722233fe697dad0b
R vd Hoek <ralvdhoek@gmail.com>
2016-05-10 19:57:35 +0200

    Update dump1090-plane-count.sh

--- dump1090-plane-count.sh
+++ dump1090-plane-count.sh
@@ -1,8 +1,10 @@
-
 #!/bin/sh
 #Count plane's dump1090 which has a handy JSON output
 
-output=$(curl -s localhost:8080/data.json)
+hostdump1090="localhost"
+port="8080"
+
+output=$(curl -s  $hostdump1090":"$port"/data.json")
 countvalid=$(echo "$output" | grep '"validposition":1' | wc -l)
 countinvalid=$(echo "$output" | grep '"validposition":0' | wc -l)
 
